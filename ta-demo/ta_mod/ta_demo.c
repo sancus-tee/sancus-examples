2235861623201fe152cba85f4b5fe6052a42f01d
Jan Tobias Muehlberg <jtmuehlberg@gmail.com>
2017-07-10 11:00:01 +0200

    Build was using the wrong compiler. fixed this.

--- ta-demo/ta_mod/ta_demo.c
+++ ta-demo/ta_mod/ta_demo.c
@@ -41,3 +41,52 @@ AUTOSTART_PROCESSES(
     &multi_threading_process
   );
 
+
+#include <string.h>
+
+int putchar(int c)
+{
+#ifdef __SANCUS_SIM
+    P1OUT = c;
+    P1OUT |= 0x80;
+    return c;
+#endif
+#ifdef __SANCUS_LOAD
+    if (c == '\n')
+        uart2_write_byte('\r');
+    uart2_write_byte(c);
+    return c;
+#endif
+}
+
+int puts_n(char *s)
+{
+  int i;
+
+  if (s) {
+    for (i = 0; i < strlen(s); i++) {
+      putchar(s[i]);
+    }
+  }
+
+  return (0);
+}
+
+void puthex_byte(uint8_t v)
+{
+  const char hexconv[] = "0123456789abcdef";
+  putchar(hexconv[v >> 4]);
+  putchar(hexconv[v & 0x0f]);
+}
+
+void puthex(uint8_t *v, int n)
+{
+  int i;
+
+  for (i = 0; i < n; i++) {
+    puthex_byte(v[i]);
+  }
+}
+
+
+
