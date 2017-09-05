c2a8d0b3978530b07b5340ae7b5f7570622cef10
Jo Van Bulck <jo.vanbulck@cs.kuleuven.be>
2017-09-05 17:57:43 +0200

    Test case to assert text section confidentiality.

--- hello-world/main.c
+++ hello-world/main.c
@@ -8,14 +8,16 @@ void exit_success(void);
 /* ======== HELLO WORLD SM ======== */
 
 DECLARE_SM(hello, 0x1234);
+#define HELLO_SECRET_CST    0xbeef
 
 int       SM_DATA(hello) hello_secret;
-int const SM_DATA(hello) hello_const = 0xbeef;
+int const SM_DATA(hello) hello_const = HELLO_SECRET_CST;
 
 void SM_FUNC(hello) hello_init(void)
 {
     /* Confidential loading guarantees secrecy of constant in text section. */
     hello_secret = hello_const;
+    ASSERT(hello_secret == HELLO_SECRET_CST);
 }
 
 void SM_ENTRY(hello) hello_greet(void)
@@ -32,9 +34,13 @@ void SM_ENTRY(hello) hello_disable(void)
 
 /* ======== UNTRUSTED CONTEXT ======== */
 
+volatile int *hello_const_pt = (volatile int *) &hello_const;
+
 int main()
 {
     msp430_io_init();
+    ASSERT((*hello_const_pt) != HELLO_SECRET_CST);
+
     sancus_enable_wrapped(&hello, SM_GET_WRAP_NONCE(hello), SM_GET_WRAP_TAG(hello));
     pr_sm_info(&hello);
 
@@ -47,13 +53,11 @@ int main()
 
 void exit_success(void)
 {
-    volatile int *p = (volatile int *) &hello_const;
-
     // TODO unprotect instruction should also clear caller ID
     //ASSERT(!sancus_get_caller_id());
     ASSERT(!sancus_get_id(hello_greet));
     ASSERT(!hello_secret);
-    ASSERT(!(*p));
+    ASSERT(!(*hello_const_pt));
 
     pr_info("SM disabled; all done!");
     EXIT();
