dbab966eca2e49e83b011e526c8178a832634f26
Hans Winderix <hans.winderix@skynet.be>
2018-12-31 12:07:15 +0100

    Make sure that the sancus examples can be compiled with the SLLVM toolchain, work in progress

--- sensor-reader/sensor.c
+++ sensor-reader/sensor.c
@@ -5,10 +5,20 @@ DECLARE_MMIO_SM(sensor, /*[secret_start, end[=*/ TSC_BASE, TSC_BASE + TSC_SIZE,
 
 sensor_data_t SM_MMIO_ENTRY(sensor) read_sensor_data(void)
 {
+#if __clang_version__ < 5
     asm("mov.b #1, %0       \n\t"
         "mov.w %1+0, r12    \n\t"
         "mov.w %1+2, r13    \n\t"
         "mov.w %1+4, r14    \n\t"
         "mov.w %1+6, r15    \n\t"
+#else
+    /* LLVM bug: See https://bugs.llvm.org/show_bug.cgi?id=39993 */
+    asm("mov.b #1, &400       \n\t"
+        "mov.w &400, r12    \n\t"
+        "mov.w &402, r13    \n\t"
+        "mov.w &404, r14    \n\t"
+        "mov.w &406, r15    \n\t"
+        "ret \n\t"
+#endif
     :: "m"(TSC_CTL), "m"(TSC_VAL):);
 }
