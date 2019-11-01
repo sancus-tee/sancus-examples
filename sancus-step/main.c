c1c624df86f0cc7699aadbfbf7976bdc5d5843cc
marton bognar <martonbognar@sch.bme.hu>
2019-11-01 15:37:18 +0100

    Readd single-stepping check to sancus-step example

--- sancus-step/main.c
+++ sancus-step/main.c
@@ -3,6 +3,12 @@
 #include <sancus_support/sm_io.h>
 #include <sancus_support/sancus_step.h>
 
+#define MAX_COUNTER (0xf)
+
+int counter = 0x0;
+int oldCounterValue = 0x0;
+int instrAfterCtr = 0;
+
 DECLARE_SM(foo, 0x1234);
 
 // The secure module has one function that behaves differently depending on
@@ -14,6 +20,22 @@ DECLARE_SM(foo, 0x1234);
 void SM_ENTRY(foo) test(char key) {
     char * p = &key;
     __asm__ __volatile__(
+        "mov #0x0, &counter\n\t"
+        "mov #0x1, &counter\n\t"
+        "mov #0x2, &counter\n\t"
+        "mov #0x3, &counter\n\t"
+        "mov #0x4, &counter\n\t"
+        "mov #0x5, &counter\n\t"
+        "mov #0x6, &counter\n\t"
+        "mov #0x7, &counter\n\t"
+        "mov #0x8, &counter\n\t"
+        "mov #0x9, &counter\n\t"
+        "mov #0xa, &counter\n\t"
+        "mov #0xb, &counter\n\t"
+        "mov #0xc, &counter\n\t"
+        "mov #0xd, &counter\n\t"
+        "mov #0xe, &counter\n\t"
+        "mov #0xf, &counter\n\t"
         "mov %0, r6\n\t"
         "mov #0x42, r7\n\t"
         "cmp.b @r6+, r7\n\t"
@@ -32,7 +54,27 @@ void SM_ENTRY(foo) test(char key) {
 
 void irqHandler(void)
 {
-    __ss_print_latency();
+    int delay = __ss_get_latency();
+    pr_info1("Latency: %d\n", delay);
+    if (counter != 0) {
+        if (!(counter == oldCounterValue || counter == oldCounterValue + 1)) {
+            pr_info("error: not single stepping\n");
+        }
+        if (counter == oldCounterValue && counter != 0 && counter != MAX_COUNTER) {
+            pr_info("warning: zero-stepping\n");
+        }
+        oldCounterValue = counter;
+        if (counter == MAX_COUNTER) {
+            if (instrAfterCtr == 6) {
+                if (delay == 1) {
+                    pr_info("Key was guessed!");
+                } else {
+                    pr_info("Key was not guessed!");
+                }
+            }
+            ++instrAfterCtr;
+        }
+    }
 }
 
 int main()
@@ -43,9 +85,19 @@ int main()
 
     __ss_start();
     test(0x41);
+    if (counter != MAX_COUNTER) {
+        pr_info2("error: invalid counter: %d - expected: %d\n", counter, MAX_COUNTER);
+    }
+
+    counter = 0;
+    oldCounterValue = 0;
+    instrAfterCtr = 0;
 
     __ss_start();
     test(0x42);
+    if (counter != MAX_COUNTER) {
+        pr_info2("error: invalid counter: %d - expected: %d\n", counter, MAX_COUNTER);
+    }
 
     EXIT();
 }
