475b44ec0dbd6068858099061e40d7c00a52cef8
marton bognar <martonbognar@sch.bme.hu>
2020-03-27 03:21:57 +0100

    Update example to use new constants, clarify description

--- dma-side-channel/main.c
+++ dma-side-channel/main.c
@@ -1,16 +1,13 @@
 #include <stdint.h>
 #include <msp430.h>
 #include <sancus/sm_support.h>
+#include <sancus_support/dma_attacker.h>
 #include <sancus_support/sm_io.h>
 #include <sancus_support/sancus_step.h>
 
-#define DELAY_BEFORE_SM 30
 #define INSTRUCTION_NUMBER_JMP 36
-
+#define TRACE_MASK 0x1000
 int instruction_counter = 0;
-uint16_t **dma_addr     = (uint16_t**) 0x0070;
-uint16_t *dma_countdown = (uint16_t*)  0x0072;
-uint16_t *dma_trace     = (uint16_t*)  0x0074;
 uint16_t delay = 0;
 
 DECLARE_SM(foo, 0x1234);
@@ -35,28 +32,30 @@ void SM_ENTRY(foo) test(char key) {
 }
 
 /*
- * For the JMP 2f instruction, the program memory trace will be 00011111 10000000
- * For the NOP    instruction, the program memory trace will be 00011111 00000000
+ * For the JMP 2f instruction, the program memory trace will be 11110000 00000000
+ * For the NOP    instruction, the program memory trace will be 11100000 00000000
+ * The JMP is instruction is executed if the guess was incorrect, the NOP if it
+ * was correct.
  *
- * By checking the different bit at the 0x40 position, we can tell which branch
- * was taken (the JPM is executed if the guess was incorrect, after the
- * bit is set).
+ * Using TRACE_MASK to get the bit that differs in the two possible traces, we
+ * can tell whether a collected trace was generated as a result of a correct or
+ *  an incorrect guess.
  */
 
 void irqHandler(void)
 {
-    delay = __ss_isr_reti_latency + DELAY_BEFORE_SM;
+    __ss_set_dma_attacker_delay(0);
     uint16_t *pmem_addr = (uint16_t*) irqHandler;
     if (instruction_counter == INSTRUCTION_NUMBER_JMP) {
-        if (*dma_trace & 0x80) {
+        if (DMA_TRACE & TRACE_MASK) {
             pr_info("Key was not guessed!");
         } else {
             pr_info("Key was guessed!");
         }
     }
     ++instruction_counter;
-    *dma_addr = pmem_addr;
-    *dma_countdown = delay;
+    DMA_ADDR = pmem_addr;
+    DMA_COUNTDOWN = delay;
 }
 
 int main()
