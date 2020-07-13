b49517288065970630315c17bc90923baf3c2bac
Jo Van Bulck <jo.vanbulck@cs.kuleuven.be>
2020-07-13 19:13:31 +0200

    improve fileio

--- fileio/ping.c
+++ fileio/ping.c
@@ -5,6 +5,17 @@
 #include <sancus_support/sm_io.h>
 #include <sancus_support/fileio.h>
 
+#define READ_BYTE(b)                        \
+    do {                                    \
+        /* Always block wait for data */    \
+        while(!fileio_available());         \
+        b = fileio_getc();                  \
+    }                                       \
+    while(0)
+
+#define WRITE_BYTE(b)                       \
+    fileio_putc(b)
+
 char *data = "Hello, world! #";
 
 int main()
@@ -16,10 +27,9 @@ int main()
 
     for (i = 0; i < strlen(data); i++)
     {
-        fileio_putc(data[i]);
-        while ((c = fileio_getc()) == EOF);
+        WRITE_BYTE(data[i]);
+        READ_BYTE(c);
         pr_info3("received %#x '%c'; sent=%#x\n", c, c, data[i]);
-        ASSERT( c != EOF);
         ASSERT( c == toupper(data[i]));
     }
 
--- fileio/pong.c
+++ fileio/pong.c
@@ -5,6 +5,17 @@
 #include <sancus_support/sm_io.h>
 #include <sancus_support/fileio.h>
 
+#define READ_BYTE(b)                        \
+    do {                                    \
+        /* Always block wait for data */    \
+        while(!fileio_available());         \
+        b = fileio_getc();                  \
+    }                                       \
+    while(0)
+
+#define WRITE_BYTE(b)                       \
+    fileio_putc(b)
+
 int main()
 {
     char c = 0;
@@ -13,13 +24,10 @@ int main()
 
     while (c != '#')
     {
-        c = fileio_getc();
-        if (c != EOF)
-        {
-            int trans = toupper(c);
-            pr_info3("received %#x '%c'; transformed= %#x\n", c, c, trans);
-            fileio_putc(trans);
-        }
+        READ_BYTE(c);
+        int trans = toupper(c);
+        pr_info3("received %#x '%c'; transformed= %#x\n", c, c, trans);
+        WRITE_BYTE(trans);
     }
 
     EXIT();
