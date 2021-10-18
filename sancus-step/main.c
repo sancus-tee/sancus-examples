300f47e6b753a246d5ff9b976467e58f9a137307
Jo Van Bulck <jo.vanbulck@cs.kuleuven.be>
2021-10-18 17:34:33 +0200

    WIP on debugging sancus-step example.

--- sancus-step/main.c
+++ sancus-step/main.c
@@ -77,6 +77,12 @@ void irqHandler(void)
     }
 }
 
+void SM_ENTRY(foo) foo_exit(void)
+{
+    /* NOTE: only SM 1 can exit on Aion */
+    FINISH();
+}
+
 int main()
 {
     msp430_io_init();
@@ -99,7 +105,7 @@ int main()
         pr_info2("error: invalid counter: %d - expected: %d\n", counter, MAX_COUNTER);
     }
 
-    __ss_dbg_exit();
+    foo_exit();
     ASSERT(0 && "should never reach here");
 }
 
